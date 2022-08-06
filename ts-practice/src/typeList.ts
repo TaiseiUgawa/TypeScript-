5e9d39ac18b2aa7745e465f33fde75c73f10adb7
https://github.com/TaiseiUgawa <taisei8808@gmai.com>
2022-08-06 14:40:08 +0900

    first

---
+++ ts-practice/src/typeList.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+// typescriptの基本的な型
+let bool: boolean = true;
+let num: number = 1;
+let str: string = "A";
+let arr1: Array<number> = [1, 2, 3];
+let arr2: number[] = [0, 1, 2];
+let any1: any = false;
+const funcA = (): void => {
+  const test = "test";
+};
+let null1: null = null;
+let undefined1: undefined = undefined;
+let object1: object = {};
+let object2: { id: number; name: string } = { id: 0, name: "" };
